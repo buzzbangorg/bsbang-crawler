42f1b6b3f928c98f0cdcf6a67f707ad9f002aa9d
Justin Clark-Casey <justincc@justincc.org>
2017-09-22 16:05:08 +0100

    parse found json+ld sections into python json structures

--- bsbang-crawler.py
+++ bsbang-crawler.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 import bs4
+import json
 import requests
 
 r = requests.get('http://localhost:8080/synbiomine/report.do?id=2026346')
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 tags = soup.find_all('script', type='application/ld+json')
 print('Found %d ld+json sections' % len(tags))
+
+jsonlds = []
+
 for tag in tags:
-    print(tag)
+    jsonlds.append(json.loads(tag.string))
+
+for jsonld in jsonlds:
+    print(jsonld)
