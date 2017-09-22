5de0fe5b36ad76bd4502497354bb0b1d26d1fdf1
Justin Clark-Casey <justincc@justincc.org>
2017-09-22 15:53:16 +0100

    find ld+json sections and print

--- bsbang-crawler.py
+++ bsbang-crawler.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import bs4
 import requests
 
 r = requests.get('http://localhost:8080/synbiomine/report.do?id=2026346')
-print(r.text)
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+tags = soup.find_all('script', type='application/ld+json')
+print('Found %d ld+json sections' % len(tags))
+for tag in tags:
+    print(tag)
