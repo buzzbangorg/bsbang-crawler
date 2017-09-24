ca67ba59f7fb4542a4658fbb50e760d6928ce79f
Justin Clark-Casey <justincc@justincc.org>
2017-09-24 19:52:39 +0100

    post retrieved json through to solr.  not yet checking we actually get an ok!

--- bsbang-crawler.py
+++ bsbang-crawler.py
@@ -4,6 +4,10 @@ import bs4
 import json
 import requests
 
+solrPath = 'http://localhost:8983/solr'
+solrJsonDocUpdatePath = solrPath + '/update/json/doc'
+
+# MAIN
 r = requests.get('http://localhost:8080/synbiomine/report.do?id=2026346')
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 tags = soup.find_all('script', type='application/ld+json')
@@ -16,3 +20,4 @@ for tag in tags:
 
 for jsonld in jsonlds:
     print(jsonld)
+    r = requests.post(solrPath, jsonld)
