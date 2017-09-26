521c1808c7bd1008dc41a65bc5ff01260d34e5e6
Justin Clark-Casey <justincc@justincc.org>
2017-09-26 17:33:56 +0100

    change bsbang-solr-setup to explicit state fields

--- bsbang-solr-setup.py
+++ bsbang-solr-setup.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
-# XXX: DON'T USE FOR NOW, WE WILL LET SOLR ADD DOCUMENTS AUTOMATICALLY USING DISCOVERED TYPES INSTEAD
-import json
 import requests
 
-solrPath = 'http://localhost:8983/solr/bsbang-dev-core/'
+solrPath = 'http://localhost:8983/solr/bsbang/'
 solrSchemaPath = solrPath + 'schema'
 
-idFieldAddJson = {'add-field': {'name': 'identifier', 'type': 'string'}}
+addFieldConfigJson = {
+    'add-field': [
+        {'name': 'identifier', 'type': 'string'},
+        {'name': 'name', 'type': 'text_en'},
+        {'name': 'additionalType', 'type': 'string'},
+        {'name': 'url', 'type': 'text_en'}
+    ]
+}
+
+addCopyFieldConfigJson = {
+    'add-copy-field': [
+        {'source': 'identifier', 'dest': '_text_'},
+        {'source': 'name', 'dest': '_text_'},
+        {'source': 'additionalType', 'dest': '_text_'},
+        {'source': 'url', 'dest': '_text_'},
+    ]
+}
+
 headers = {'Content-type': 'application/json'}
 
-r = requests.post(solrSchemaPath, data=json.dumps(idFieldAddJson), headers=headers)
+r = requests.post(solrSchemaPath, json=addFieldConfigJson, headers=headers)
+print('response [%s]' % r.text)
+
+r = requests.post(solrSchemaPath, json=addCopyFieldConfigJson, headers=headers)
 print('response [%s]' % r.text)
