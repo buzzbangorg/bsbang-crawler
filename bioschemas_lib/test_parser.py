62e6b4d795f03a5a9744b11d474f8254fad0063f
Justin Clark-Casey <justincc@justincc.org>
2017-09-29 18:12:34 +0100

    Add test_load_bioschemas_jsonld_from_html()

--- bioschemas_lib/test_parser.py
+++ bioschemas_lib/test_parser.py
@@ -6,6 +6,23 @@ from bioschemas_lib.parser import BioschemasParser
 
 
 class TestBioschemasParserMethods(unittest.TestCase):
+    def test_load_bioschemas_jsonld_from_html(self):
+        html = '''<script type="application/ld+json">
+        {
+          "@context": "http://bioschemas.org",
+          "@type": "PhysicalEntity",
+          "name": "Gene arcA E. coli str. K-12 substr. MG1655 b4401",
+          "additionalType": "http://www.ebi.ac.uk/ols/ontologies/so/terms?obo_id=SO:0000704",
+          "identifier": "b4401",
+          "url": "http://localhost:8080/synbiomine/report.do?id=2026346"
+        }
+        </script>
+        '''
+
+        bs_parser = BioschemasParser()
+        jsonlds = bs_parser.parse_bioschemas_jsonld_from_html(html)
+        self.assertEqual(len(jsonlds), 1)
+
     def test_load_bioschemas_jsonld_from_html_mandatory_prop_missing(self):
         # "name": "Gene arcA E. coli str. K-12 substr. MG1655 b4401",
         html = '''<script type="application/ld+json">
