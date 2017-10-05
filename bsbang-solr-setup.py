fb6562337999de304e5a54d48b417c2cb8e620ae
Justin Clark-Casey (justincc) <justinccdev@gmail.com>
2017-10-05 10:04:32 +0100

    load setup fields from a separate config file

--- bsbang-solr-setup.py
+++ bsbang-solr-setup.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
 
+import argparse
+from lxml import etree
 import requests
 
 solrPath = 'http://localhost:8983/solr/bsbang/'
 solrSchemaPath = solrPath + 'schema'
 
-addFieldConfigJson = {
-    'add-field': [
-        {'name': 'identifier', 'type': 'string'},
-        {'name': 'name', 'type': 'text_en'},
-        {'name': 'additionalType', 'type': 'string'},
-        {'name': 'url', 'type': 'text_en'}
-    ]
-}
+# MAIN
+parser = argparse.ArgumentParser('Setup a Solr schema for Buzzbang')
+parser.add_argument('config', help='Config file location, e.g. conf/bsbang-solr-setup.xml')
+args = parser.parse_args()
 
-addCopyFieldConfigJson = {
-    'add-copy-field': [
-        {'source': 'identifier', 'dest': '_text_'},
-        {'source': 'name', 'dest': '_text_'},
-        {'source': 'additionalType', 'dest': '_text_'},
-        {'source': 'url', 'dest': '_text_'},
-    ]
-}
+configXml = etree.parse(args.config)
 
-headers = {'Content-type': 'application/json'}
+for fieldElem in configXml.findall('./field'):
+    print(fieldElem.attrib['name'] + ' ' + fieldElem.attrib['type'])
+    addFieldConfigJson = {
+        'add-field': {'name': fieldElem.attrib['name'], 'type': fieldElem.attrib['type']}
+    }
 
-r = requests.post(solrSchemaPath, json=addFieldConfigJson, headers=headers)
-print('response [%s]' % r.text)
+    headers = {'Content-type': 'application/json'}
 
-r = requests.post(solrSchemaPath, json=addCopyFieldConfigJson, headers=headers)
-print('response [%s]' % r.text)
+    r = requests.post(solrSchemaPath, json=addFieldConfigJson, headers=headers)
+    print('response [%s]' % r.text)
+
+    addCopyFieldConfigJson = {
+        'add-copy-field': {'source': fieldElem.attrib['name'], 'dest': '_text_'}
+    }
+
+    r = requests.post(solrSchemaPath, json=addCopyFieldConfigJson, headers=headers)
+    print('response [%s]' % r.text)
