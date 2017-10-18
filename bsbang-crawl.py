dae2bbe7ca834035fe39cfda96fc97aa4684eac6
Justin Clark-Casey <justincc@justincc.org>
2017-10-18 21:04:06 +0100

    Move bsbang specific crawling functions to bsbang module, in preparation for unit testing

--- bsbang-crawl.py
+++ bsbang-crawl.py
@@ -1,66 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-import canonicaljson
-import hashlib
 import os
-import requests
 
-import bioschemas
-import bioschemas.crawler
-import bioschemas.parser
-import bioschemas.translator
-
-
-def load_bioschemas_jsonld_from_url(url):
-    """
-    Load Bioschemas JSON-LD from an url.  This may be a webpage or a sitemap pointing to webpages'''
-
-    :param url:
-    :return:
-    """
-
-    if url.endswith('/sitemap.xml'):
-        urls = bioschemas.crawler.get_urls_from_sitemap(url)
-        urlsLen = len(urls)
-        i = 1
-        for url in urls:
-            print('Crawling %d of %d pages' % (i, urlsLen))
-            load_bioschemas_jsonld_from_html(url)
-            i += 1
-    else:
-        load_bioschemas_jsonld_from_html(url)
-
-
-def load_bioschemas_jsonld_from_html(url):
-    """
-    Load Bioschemas JSON-LD from a webpage.
-
-    :param url:
-    :return:
-    """
-
-    try:
-        parser = bioschemas.parser.BioschemasParser()
-        jsonlds = parser.parse_bioschemas_jsonld_from_url(url)
-
-        headers = {'Content-type': 'application/json'}
-
-        for jsonld in jsonlds:
-            schema = jsonld['@type']
-            solr_json = bioschemas.translator.create_solr_json_with_mandatory_properties(schema, jsonld)
-
-            # TODO: Use solr de-dupe for this
-            # jsonld['id'] = str(uuid.uuid5(namespaceUuid, json.dumps(jsonld)))
-            solr_json['id'] = hashlib.sha256(canonicaljson.encode_canonical_json(solr_json)).hexdigest()
-
-            print(solr_json)
-
-            if config['post_to_solr']:
-                r = requests.post(config['solr_json_doc_update_path'] + '?commit=true', json=solr_json, headers=headers)
-                print(r.text)
-    except Exception as e:
-        print('Ignoring failure with %s' % str(e))
+import bsbang
 
 # MAIN
 parser = argparse.ArgumentParser('Crawl a sitemap XML or webpage and insert the bioschemas information into Solr.')
@@ -83,6 +26,6 @@ if os.path.exists(args.location):
         for line in f:
             line = line.strip()
             if not line.startswith('#'):
-                load_bioschemas_jsonld_from_url(line)
+                bsbang.load_bioschemas_jsonld_from_url(line, config)
 else:
-    load_bioschemas_jsonld_from_url(args.location)
+    bsbang.load_bioschemas_jsonld_from_url(args.location, config)
---
+++ bsbang/__init__.py
@@ -0,0 +1,61 @@
+import canonicaljson
+import hashlib
+import requests
+
+import bioschemas
+import bioschemas.crawler
+import bioschemas.parser
+import bioschemas.translator
+
+
+def load_bioschemas_jsonld_from_url(url, config):
+    """
+    Load Bioschemas JSON-LD from an url.  This may be a webpage or a sitemap pointing to webpages'''
+
+    :param url:
+    :param config:
+    :return:
+    """
+
+    if url.endswith('/sitemap.xml'):
+        urls = bioschemas.crawler.get_urls_from_sitemap(url)
+        urls_len = len(urls)
+        i = 1
+        for url in urls:
+            print('Crawling %d of %d pages' % (i, urls_len))
+            load_bioschemas_jsonld_from_html(url, config)
+            i += 1
+    else:
+        load_bioschemas_jsonld_from_html(url, config)
+
+
+def load_bioschemas_jsonld_from_html(url, config):
+    """
+    Load Bioschemas JSON-LD from a webpage.
+
+    :param url:
+    :param config:
+    :return:
+    """
+
+    try:
+        parser = bioschemas.parser.BioschemasParser()
+        jsonlds = parser.parse_bioschemas_jsonld_from_url(url)
+
+        headers = {'Content-type': 'application/json'}
+
+        for jsonld in jsonlds:
+            schema = jsonld['@type']
+            solr_json = bioschemas.translator.create_solr_json_with_mandatory_properties(schema, jsonld)
+
+            # TODO: Use solr de-dupe for this
+            # jsonld['id'] = str(uuid.uuid5(namespaceUuid, json.dumps(jsonld)))
+            solr_json['id'] = hashlib.sha256(canonicaljson.encode_canonical_json(solr_json)).hexdigest()
+
+            print(solr_json)
+
+            if config['post_to_solr']:
+                r = requests.post(config['solr_json_doc_update_path'] + '?commit=true', json=solr_json, headers=headers)
+                print(r.text)
+    except Exception as e:
+        print('Ignoring failure with %s' % str(e))
