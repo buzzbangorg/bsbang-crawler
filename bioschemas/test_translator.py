dc7ff0e33f763243ea8cb25bf0764fa00536b1e8
Justin Clark-Casey <justincc@justincc.org>
2017-11-17 17:14:18 +0000

    simplify TestTranslator.test_translate to use test specific types and properties rather than the bioschemas ones

--- bioschemas/test_translator.py
+++ bioschemas/test_translator.py
@@ -7,29 +7,31 @@ import bioschemas.translator
 logging.basicConfig(level=logging.DEBUG)
 
 
-class TestTranslatorMethods(unittest.TestCase):
+class TestTranslator(unittest.TestCase):
     def test_translate(self):
         config = {
-            'jsonld_to_solr_map': bioschemas.JSONLD_TO_SOLR_MAP,
-            'mandatory_properties': bioschemas.MANDATORY_PROPERTIES,
-            'schema_inheritance_graph': bioschemas.SCHEMA_INHERITANCE_GRAPH,
-            'schemas_to_parse': bioschemas.SCHEMAS_TO_PARSE
+            'jsonld_to_solr_map': {'@type': 'AT_type'},
+
+            'mandatory_properties': {
+                'Parent_Type': ['@type', 'parent_prop'],
+                'Child_type': ['child_prop']
+            },
+
+            'schema_inheritance_graph': {
+                'Child_type': 'Parent_Type',
+                'Parent_Type': None
+            }
         }
 
         translator = bioschemas.translator.Translator(config)
 
         jsonld = {
-            'context': 'http://bioschemas.org',
-            '@type': 'PhysicalEntity',
-            'identifier': 'b4401',
-            'additionalType': 'http://www.ebi.ac.uk/ols/ontologies/so/terms?obo_id=SO:0000704',
-            'name': 'Gene arcA E. coli str. K-12 substr. MG1655 b4401',
-            'url': 'http://localhost:8080/synbiomine/report.do?id=2026346'
+            '@type': 'Child_type',
+            'parent_prop': 'pp_val',
+            'child_prop': 'cp_val'
         }
 
         solr_json = translator.create_solr_json_with_mandatory_properties(jsonld['@type'], jsonld)
-        self.assertEqual(solr_json['name'], 'Gene arcA E. coli str. K-12 substr. MG1655 b4401')
-        self.assertEqual(solr_json['AT_type'], 'PhysicalEntity')
-        self.assertEqual(solr_json['additionalType'], 'http://www.ebi.ac.uk/ols/ontologies/so/terms?obo_id=SO:0000704')
-        self.assertEqual(solr_json['url'], 'http://localhost:8080/synbiomine/report.do?id=2026346')
-        self.assertFalse('context' in solr_json)
+        self.assertEqual(solr_json['parent_prop'], 'pp_val')
+        self.assertEqual(solr_json['child_prop'], 'cp_val')
+        self.assertEqual(solr_json['AT_type'], 'Child_type')
