bd2700a4a4accd7b9f0f62ee31f094588663dbcd
Justin Clark-Casey <justincc@justincc.org>
2017-11-29 18:29:52 +0000

    Make it possible for the crawler to load sitemaps from https urls and those without xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" in <sitemap>

--- bioschemas/crawler.py
+++ bioschemas/crawler.py
@@ -1,17 +1,20 @@
 import logging
 
+import io
 import lxml.etree as etree
+import requests
 
 
 def get_urls_from_sitemap(sitemap_url):
     """Get all the webpage urls we can reach from a sitemap, whether this is a sitemap XML or a sitemap index XML"""
-    sitemap = etree.parse(sitemap_url)
+    r = requests.get(sitemap_url)
+    sitemap = etree.parse(io.BytesIO(r.content))
     root_tag = sitemap.getroot().tag
 
-    if root_tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}sitemapindex':
+    if root_tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}sitemapindex' or root_tag == 'sitemapindex':
         logging.info('Loading sitemap index %s' % sitemap_url)
         return get_urls_from_loaded_sitemapindex(sitemap)
-    elif root_tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}urlset':
+    elif root_tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}urlset' or root_tag == 'urlset':
         logging.info('Loading sitemap %s' % sitemap_url)
         return get_urls_from_loaded_sitemap(sitemap)
     else:
@@ -21,6 +24,8 @@ def get_urls_from_sitemap(sitemap_url):
 def get_urls_from_loaded_sitemap(sitemap):
     urls = set()
     loc_elems = sitemap.findall('//{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
+    loc_elems += sitemap.findall('//loc')
+
     loc_elems_len = len(loc_elems)
     logging.info('Found %d pages to crawl' % loc_elems_len)
     for loc_elem in loc_elems:
@@ -34,6 +39,8 @@ def get_urls_from_loaded_sitemapindex(sitemapindex):
     urls = set()
     # for loc_elem in sitemapindex_elem.findall('/sitemap/loc'):
     for loc_elem in sitemapindex.findall('//{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
+        urls.update(get_urls_from_sitemap(loc_elem.text))
+    for loc_elem in sitemapindex.findall('//loc'):
         urls.update(get_urls_from_sitemap(loc_elem.text))
 
     return urls
