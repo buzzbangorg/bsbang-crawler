40b861aff908ffb8c78fda25a5d23b162798c520
Justin Clark-Casey <justincc@justincc.org>
2017-11-29 17:54:57 +0000

    add ability to specific multiValued in bsbang-solr-setup.py

--- bsbang-solr-setup.py
+++ bsbang-solr-setup.py
@@ -7,6 +7,15 @@ import requests
 solrPath = 'http://localhost:8983/solr/bsbang/'
 solrSchemaPath = solrPath + 'schema'
 
+
+def post_to_solr(json):
+    headers = {'Content-type': 'application/json'}
+
+    print('posting [%s]' % json)
+    r = requests.post(solrSchemaPath, json=json, headers=headers)
+    print('response [%s]' % r.text)
+
+
 # MAIN
 parser = argparse.ArgumentParser('Setup a Solr schema for Buzzbang')
 parser.add_argument('config', help='Config file location, e.g. conf/bsbang-solr-setup.xml')
@@ -17,17 +26,18 @@ configXml = etree.parse(args.config)
 for fieldElem in configXml.findall('./field'):
     print(fieldElem.attrib['name'] + ' ' + fieldElem.attrib['type'])
     addFieldConfigJson = {
-        'add-field': {'name': fieldElem.attrib['name'], 'type': fieldElem.attrib['type']}
+        'add-field': {
+            'name': fieldElem.attrib['name'],
+            'type': fieldElem.attrib['type'],
+            'multiValued': fieldElem.get('multiValued', default='false')
+        }
     }
 
-    headers = {'Content-type': 'application/json'}
-
-    r = requests.post(solrSchemaPath, json=addFieldConfigJson, headers=headers)
-    print('response [%s]' % r.text)
+    post_to_solr(addFieldConfigJson)
 
     addCopyFieldConfigJson = {
         'add-copy-field': {'source': fieldElem.attrib['name'], 'dest': '_text_'}
     }
 
-    r = requests.post(solrSchemaPath, json=addCopyFieldConfigJson, headers=headers)
-    print('response [%s]' % r.text)
+    post_to_solr(addCopyFieldConfigJson)
+    print()
