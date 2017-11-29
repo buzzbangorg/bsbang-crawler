buzzbangorg/bsbang-crawler

commit ac66a4a44322b9fa94827f9713c018b6ab70ddf8

more sensibly make the default bioschemas config the same structure as the one we shuttle about internally

bioschemas/__init__.py
@@ -1,25 +1,28 @@
-# The properties that must exist on schemas for us to accept them for indexing
-MANDATORY_PROPERTIES = {
-    'Thing': ['@type', 'name', 'url'],
-    'DataCatalog': ['description', 'keywords'],
-    'PhysicalEntity': ['additionalType']
+DEFAULT_CONFIG = {
+
+    # The properties that must exist on schemas for us to accept them for indexing
+    'mandatory_properties': {
+        'Thing': ['@type', 'name', 'url'],
+        'DataCatalog': ['description', 'keywords'],
+        'PhysicalEntity': ['additionalType']
+    },
+
+    # The properties that will be indexed if present
+    'optional_properties': {
+        'Thing': ['alternateName']
+    },
+
+    # The inheritance graph of the schemas that we care about
+    'schema_inheritance_graph': {
+        'CreativeWork': 'Thing',
+        'DataCatalog': 'CreativeWork',
+        'PhysicalEntity': 'Thing',
+        'Thing': None
+    },
+
+    # The schemas that we want to index
+    'schemas_to_parse': ['DataCatalog', 'PhysicalEntity'],
+
+    # If a type is not in the map, then assume the source and target names are the same
+    'jsonld_to_solr_map': {'@type': 'AT_type'},
 }
-
-# The properties that will be indexed if present
-OPTIONAL_PROPERTIES = {
-    'Thing': ['alternateName']
-}
-
-# The inheritance graph of the schemas that we care about
-SCHEMA_INHERITANCE_GRAPH = {
-    'CreativeWork': 'Thing',
-    'DataCatalog': 'CreativeWork',
-    'PhysicalEntity': 'Thing',
-    'Thing': None
-}
-
-# The schemas that we want to index
-SCHEMAS_TO_PARSE = ['DataCatalog', 'PhysicalEntity']
-
-# If a type is not in the map, then assume the source and target names are the same
-JSONLD_TO_SOLR_MAP = {'@type': 'AT_type'}

bsbang-crawl.py
@@ -20,16 +20,11 @@ If given a path (e.g. conf/default-targets.txt) then crawl all the newline-separ
 parser.add_argument('--nosolr', action='store_true', help='Don''t actually load anything into Solr, just fetch')
 args = parser.parse_args()
 
-config = {
-    'jsonld_to_solr_map': bioschemas.JSONLD_TO_SOLR_MAP,
-    'mandatory_properties': bioschemas.MANDATORY_PROPERTIES,
-    'optional_properties': bioschemas.OPTIONAL_PROPERTIES,
-    'schema_inheritance_graph': bioschemas.SCHEMA_INHERITANCE_GRAPH,
-    'schemas_to_parse': bioschemas.SCHEMAS_TO_PARSE,
-
+config = bioschemas.DEFAULT_CONFIG.copy()
+config.update({
     'post_to_solr': not args.nosolr,
     'solr_json_doc_update_path': 'http://localhost:8983/solr/bsbang/update/json/docs'
-}
+})
 
 if os.path.exists(args.location):
     with open(args.location) as f:

commit 06bdbf6dad823e5c3042a95ebd6cf13aa79913d0

Fix test_parser tests I broke in ac66a4a

bioschemas/__init__.py
@@ -23,6 +23,9 @@ DEFAULT_CONFIG = {
     # The schemas that we want to index
     'schemas_to_parse': ['DataCatalog', 'PhysicalEntity'],
 
+    # To capture older Bioschemas markup, we want to map some older schemas onto newer ones
+    'schema_map': {'BiologicalEntity': 'PhysicalEntity'},
+
     # If a type is not in the map, then assume the source and target names are the same
     'jsonld_to_solr_map': {'@type': 'AT_type'},
 }

bioschemas/test_parser.py
@@ -3,12 +3,7 @@ import unittest
 import bioschemas
 from bioschemas.parser import Parser
 
-config = {
-    'jsonld_to_solr_map': bioschemas.JSONLD_TO_SOLR_MAP,
-    'mandatory_properties': bioschemas.MANDATORY_PROPERTIES,
-    'schema_inheritance_graph': bioschemas.SCHEMA_INHERITANCE_GRAPH,
-    'schemas_to_parse': bioschemas.SCHEMAS_TO_PARSE
-}
+config = bioschemas.DEFAULT_CONFIG
 
 
 class TestParser(unittest.TestCase):