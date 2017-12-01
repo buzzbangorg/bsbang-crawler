bc72805dac86ea2c51f5c176c2dc28e81d4e992e
Justin Clark-Casey <justincc@justincc.org>
2017-12-01 18:13:52 +0000

    add TestFilters.test_non_bs_type()

--- bioschemas/test_filters.py
+++ bioschemas/test_filters.py
@@ -6,13 +6,27 @@ import bioschemas.filters
 class TestFilters(unittest.TestCase):
     def test_bs_mandatory_prop_missing(self):
         config = {
-            'mandatory_properties': {'type', 'mandatory_prop'},
-            'schema_inheritance_graph': {'type': None}
+            'mandatory_properties': {'atype', 'mandatory_prop'},
+            'schema_inheritance_graph': {'atype': None}
         }
 
         jsonld = {
-            '@type': 'type',
+            '@type': 'atype',
             'some_other_prop': 'cheese'
+        }
+
+        f = bioschemas.filters.BioschemasFilter(config)
+        jsonlds = f.filter([jsonld])
+        self.assertEqual(len(jsonlds), 0)
+
+    def test_non_bs_type(self):
+        config = {
+            'mandatory_properties': {'atype', 'mandatory_prop'},
+            'schema_inheritance_graph': {'atype': None}
+        }
+
+        jsonld = {
+            '@type': 'btype',
         }
 
         f = bioschemas.filters.BioschemasFilter(config)
