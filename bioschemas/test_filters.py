a757fce298baa2de6b00277f4f1dad3919d224e1
Justin Clark-Casey <justincc@justincc.org>
2017-12-01 18:14:34 +0000

    change names of TestFilters tests since these aren't bs specific anymore

--- bioschemas/test_filters.py
+++ bioschemas/test_filters.py
@@ -4,7 +4,21 @@ import bioschemas.filters
 
 
 class TestFilters(unittest.TestCase):
-    def test_bs_mandatory_prop_missing(self):
+    def test_ignored_type(self):
+        config = {
+            'mandatory_properties': {'atype', 'mandatory_prop'},
+            'schema_inheritance_graph': {'atype': None}
+        }
+
+        jsonld = {
+            '@type': 'btype',
+        }
+
+        f = bioschemas.filters.BioschemasFilter(config)
+        jsonlds = f.filter([jsonld])
+        self.assertEqual(len(jsonlds), 0)
+
+    def test_mandatory_prop_missing(self):
         config = {
             'mandatory_properties': {'atype', 'mandatory_prop'},
             'schema_inheritance_graph': {'atype': None}
@@ -13,20 +27,6 @@ class TestFilters(unittest.TestCase):
         jsonld = {
             '@type': 'atype',
             'some_other_prop': 'cheese'
-        }
-
-        f = bioschemas.filters.BioschemasFilter(config)
-        jsonlds = f.filter([jsonld])
-        self.assertEqual(len(jsonlds), 0)
-
-    def test_non_bs_type(self):
-        config = {
-            'mandatory_properties': {'atype', 'mandatory_prop'},
-            'schema_inheritance_graph': {'atype': None}
-        }
-
-        jsonld = {
-            '@type': 'btype',
         }
 
         f = bioschemas.filters.BioschemasFilter(config)
